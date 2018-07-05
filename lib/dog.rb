9b89ee99200f5332e946ddfb86e9bca83290e20b
Justine Bricknell <justinegayton@gmail.com>
2018-07-05 21:37:26 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,11 +3,13 @@ class Dog
   @@all = []
 
   def initialize(name)
-    @name = name 
+    @name = name
     @@all << self
   end
 
   def self.all
+    @@all.each do |name|
+      puts @@all 
   end
 
   def self.clear_all
